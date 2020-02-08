772474c57cc2bb011b22d557a73ddd87788cda47
lfarrel6 <lfarrel6@tcd.ie>
2020-02-08 17:21:37 +0000

    Updates

---
+++ precache-manifest.427c1184eefd8971659baceffa037c7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f8b7fc53c99da85e28f5c9423bc420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5d665cb4302291f1c8",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "738ea0db3442f53ef21f",
+    "url": "/static/css/main.fefb1e37.chunk.css"
+  },
+  {
+    "revision": "8a5d665cb4302291f1c8",
+    "url": "/static/js/2.73732d86.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.73732d86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "738ea0db3442f53ef21f",
+    "url": "/static/js/main.df2d8538.chunk.js"
+  },
+  {
+    "revision": "b3deee908da4b7211ad5",
+    "url": "/static/js/runtime-main.65c6d97b.js"
+  },
+  {
+    "revision": "b3721ba3bde34e5b38b0e1523cccfd7f",
+    "url": "/static/media/PlayfairDisplay-Regular.b3721ba3.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/static/media/Raleway-Regular.9942588a.ttf"
+  }
+]);
